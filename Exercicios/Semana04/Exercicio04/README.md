5c59ec491543515a7c5fe42d72a822ad6655cbb0
Caetano Braga <caetanobraga@gmail.com>
2022-07-24 22:27:40 -0300

    Exercicio 04 semana 04

---
+++ Exercicios/Semana04/Exercicio04/README.md
@@ -0,0 +1,10 @@
+**[M1S04] Ex 4 - Conta bancaria Parte 4**
+
+- Vamos criar a função de saque que deve ser executada caso a opção selecionada no select seja saque
+
+  1. Crie uma função para sacar dinheiro que deve receber como parâmetro o valor a ser sacado e o id da conta;
+  1.1. Se o valor for menor ou igual zero deve exibir a mensagem de valor inválido e não realizar nenhuma operação;
+  1.2. Se o valor for maior que o saldo em conta deve exibir a mensagem de saldo insuficiente e exibir o saldo atual;
+  1.3. Se o valor for maior que zero e menor que o saldo em conta deve exibir a mensagem informado que o saque ocorreu com sucesso e o saldo atual da conta após o saque, além de atualizar o array com esse novo valor;
+
+  _Dica: Dê uma olhadinha em find, map, if..._
---
+++ Exercicios/Semana04/Exercicio04/index.js
@@ -0,0 +1,113 @@
+let contasClientes = [
+    {
+        id: 1,
+        nome: 'Cliente 01',
+        saldo: 500,
+    },
+    {
+        id: 2,
+        nome: 'Cliente 02',
+        saldo: 3000,
+    },
+    {
+        id: 3,
+        nome: 'Cliente 03',
+        saldo: 5000,
+    },
+    {
+        id: 4,
+        nome: 'Cliente 04',
+        saldo: 5000,
+    },
+    {
+        id: 5,
+        nome: 'Cliente 05',
+        saldo: 3000,
+    },
+    {
+        id: 6,
+        nome: 'Cliente 06',
+        saldo: 5000,
+    },
+    {
+        id: 7,
+        nome: 'Cliente 07',
+        saldo: 5000,
+    },
+];
+
+const body = document.body;
+const selectClientes = document.getElementById("contasselect");
+const form = document.getElementById("form");
+const strongMensagem = document.getElementById("mensagem");
+
+const populaSelect = ()=> {
+    for(cliente in contasClientes) {
+        option = new Option(contasClientes[cliente].nome, contasClientes[cliente].id);
+        selectClientes.options[selectClientes.options.length] = option;      
+  }
+}
+
+body.onload = populaSelect;
+
+const exibeMensagem = (mensagem, type = 'error') => {
+    strongMensagem.textContent = mensagem;
+    strongMensagem.className = type === 'error'?'error':'success';
+
+}
+
+const fazSaque = (contaAtual, valor) => {
+    if(isNaN(valor) || valor <= 0 ){
+        //exibeMensagem('Valor inválido');
+        return;
+    };
+
+    if (valor > contaAtual.saldo){
+        exibeMensagem(`Saldo insuficiente. Saldo atual: ${contaAtual.saldo}`);
+        return;
+    };
+
+    const novoSaldo = contaAtual.saldo - valor;
+
+
+    const contaSemContaAtual = contasClientes.filter((account) => account.id !== contaAtual.id);
+    console.log('contas Sem conta atual', contaSemContaAtual);
+    console.log('conta atual', contaAtual);
+    const contasAtualizadas = [...contaSemContaAtual, {...contaAtual, saldo: novoSaldo}];
+    contasClientes = contasAtualizadas;
+
+    exibeMensagem(`Saque efetuado com sucesso! Saldo atual: ${novoSaldo}`, 'success');
+};
+
+const fazDeposito = (contaAtual, valor) => {
+    if(isNaN(valor) || valor <= 0 ){
+        return;
+    };
+
+    if (valor > contaAtual.saldo){
+        exibeMensagem(`Saldo insuficiente. Saldo atual: ${contaAtual.saldo}`);
+        return;
+    };
+
+    const novoSaldo = contaAtual.saldo - valor;
+
+
+    const contaSemContaAtual = contasClientes.filter((account) => account.id !== contaAtual.id);
+    console.log('contas Sem conta atual', contaSemContaAtual);
+    console.log('conta atual', contaAtual);
+    const contasAtualizadas = [...contaSemContaAtual, {...contaAtual, saldo: novoSaldo}];
+    contasClientes = contasAtualizadas;
+
+    exibeMensagem(`Saque efetuado com sucesso! Saldo atual: ${novoSaldo}`, 'success');
+};
+
+
+
+
+const operacoes = (event) => {
+    event.preventDefault()
+
+    fazSaque(contasClientes[0], 200);
+}
+
+form.onsubmit = operacoes;
