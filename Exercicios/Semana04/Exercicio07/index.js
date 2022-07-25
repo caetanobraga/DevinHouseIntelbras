d9c184e45cdb95c6c231445aa282f283968981d8
Caetano Braga <caetanobraga@gmail.com>
2022-07-25 00:48:29 -0300

    Exercicio 07 semana 04

---
+++ Exercicios/Semana04/Exercicio07/index.js
@@ -0,0 +1,137 @@
+let contasClientes = [
+    {
+        id: 1,
+        nome: 'Cliente 01',
+        saldo: 500,
+        senha: 1234,
+    },
+    {
+        id: 2,
+        nome: 'Cliente 02',
+        saldo: 3000,
+        senha: 5678,
+    },
+    {
+        id: 3,
+        nome: 'Cliente 03',
+        saldo: 5000,
+        senha: 9012,
+    },
+    {
+        id: 4,
+        nome: 'Cliente 04',
+        saldo: 3456,
+    },
+    {
+        id: 5,
+        nome: 'Cliente 05',
+        saldo: 7890,
+    },
+    {
+        id: 6,
+        nome: 'Cliente 06',
+        saldo: 2122,
+    },
+    {
+        id: 7,
+        nome: 'Cliente 07',
+        saldo: 2324,
+    },
+]
+
+const body = document.body;
+const selectClientes = document.getElementById("contasselect");
+const form = document.getElementById("form");
+const strongMensagem = document.getElementById("mensagem");
+
+const populaSelect = ()=> {
+    for(cliente in contasClientes) {
+        option = new Option(contasClientes[cliente].nome, contasClientes[cliente].id);
+        selectClientes.options[selectClientes.options.length] = option;      
+  }
+}
+
+body.onload = populaSelect;
+
+const exibeMensagem = (mensagem, type = 'error') => {
+    strongMensagem.textContent = mensagem;
+    strongMensagem.className = type === 'error'?'error':'success';
+}
+
+const validaValor = (valor) => {
+    if(isNaN(valor) || valor <= 0 ){
+        exibeMensagem('Valor inválido')
+        return false;
+    }
+    return true;
+}
+
+const atualizaSaldo = (contaAtual, saldo) => {
+    const contaSemContaAtual = contasClientes.filter((account) => account.id !== contaAtual.id);
+    const contasAtualizadas = [...contaSemContaAtual, {...contaAtual, saldo}];
+    contasClientes = contasAtualizadas;
+}
+
+const fazSaque = (contaAtual, valor) => {
+    if(!validaValor(valor)){
+        return;
+    }
+
+    if (valor > contaAtual.saldo){
+        exibeMensagem(`Saldo insuficiente. Saldo atual: ${contaAtual.saldo}`);
+        return;
+    }
+    const novoSaldo = contaAtual.saldo - valor;
+    atualizaSaldo(contaAtual, novoSaldo);
+    exibeMensagem(`Saque efetuado com sucesso! Saldo atual: ${novoSaldo}`, 'success');
+}
+
+const fazDeposito = (contaAtual, valor) => {
+    if(!validaValor(valor)){
+        return;
+    }
+    const novoSaldo = contaAtual.saldo + valor;
+    atualizaSaldo(contaAtual, novoSaldo);
+    exibeMensagem(`Deposito efetuado com sucesso! Saldo atual: ${novoSaldo}`, 'success');
+}
+
+const operacoes = (event) => {
+    event.preventDefault()
+    const contaId = parseInt(event.target.contasselect.value);
+    const valor = parseFloat(event.target.valor.value);
+    const operacao = parseInt(event.target.operacao.value);
+    const senha = event.target.senha.value;
+
+    if(!validaValor(contaId) || !validaValor(valor) || !validaValor(operacao)){
+        return;
+    }
+
+    if (!senha){
+        exibeMensagem('Informe a senha')
+        return
+    }
+
+    const contaAtual = contasClientes.find((conta) => conta.id === contaId);
+    if (!contaAtual){
+        exibeMensagem('Conta inválida!');
+    }
+
+    if (contaAtual.senha !== parseInt(senha)){
+        exibeMensagem('Senha inválida!');
+        return
+    }
+
+    switch (operacao) {
+        case 1:
+            fazSaque(contaAtual, valor)
+            break;
+        case 2:
+            fazDeposito(contaAtual, valor)
+            break;
+        default:
+            exibeMensagem('Operacao invalida');
+            break;
+    }
+}
+
+form.onsubmit = operacoes;
