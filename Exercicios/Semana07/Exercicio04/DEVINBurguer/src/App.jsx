24a2770308c33bc125fc0849e52fe7e7d92ef24f
Caetano Braga <caetanobraga@gmail.com>
2022-08-27 21:57:10 -0300

    exercicio 2 semana 8

--- Exercicios/Semana07/Exercicio04/DEVINBurguer/src/App.jsx
+++ Exercicios/Semana07/Exercicio04/DEVINBurguer/src/App.jsx
@@ -11,61 +11,54 @@ function App() {
     <div className="container">
      <Navbar/>
         <div className='itensMenu'>
-          <div className='secao'>
-            <h2>Lanches</h2>
-            <hr></hr>
-          </div>
-          <div className='subsecao'>
-            <h3>Lanches prensados</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Lanches" pratos={pratos.Lanches} subcategoria={"Xis"}/>
-          <div className='subsecao'>
-            <h3>Porções</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Lanches" pratos={pratos.Lanches} subcategoria={"porcoes"}/>
-
-          <div className='secao'>
-            <h2>Pratos</h2>
-            <hr></hr>
-          </div>
-          <div className='subsecao'>
-            <h3>Comida brasileira</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Pratos" pratos={pratos.pratos}  subcategoria={"brasileira"}/>
-
-          <div className='subsecao'>
-            <h3>Massas</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Pratos" pratos={pratos.pratos}  subcategoria={"massas"}/>
-
-
-          <div className='secao'>
-            <h2>Bebidas</h2>
-            <hr></hr>
-          </div>
-          <div className='subsecao'>
-            <h3>Refrigerantes</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Bebidas" pratos={pratos.bebidas} subcategoria={"refrigerantes"}/>
-
-
-          <div className='subsecao'>
-            <h3>Cervejas</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Bebidas" pratos={pratos.bebidas} subcategoria={"cervejas"}/>
-
-          <div className='subsecao'>
-            <h3>Vinhos</h3>
-            <hr></hr>
-          </div>
-          <Prato nome="Bebidas" pratos={pratos.bebidas} subcategoria={"vinhos"}/>
-
+          
+            <div className='secao'>
+              <h2>Lanches</h2>
+              <hr></hr>
+            </div>
+            <div className='subsecao'>
+              <h3>Lanches prensados</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.Lanches} subcategoria={"Xis"}/>
+            <div className='subsecao'>
+              <h3>Porções</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.Lanches} subcategoria={"porcoes"}/>
+            <div className='secao'>
+              <h2>Pratos</h2>
+              <hr></hr>
+            </div>
+            <div className='subsecao'>
+              <h3>Comida brasileira</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.pratos}  subcategoria={"brasileira"}/>
+            <div className='subsecao'>
+              <h3>Massas</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.pratos}  subcategoria={"massas"}/>
+            <div className='secao'>
+              <h2>Bebidas</h2>
+              <hr></hr>
+            </div>
+            <div className='subsecao'>
+              <h3>Refrigerantes</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.bebidas} subcategoria={"refrigerantes"}/>
+            <div className='subsecao'>
+              <h3>Cervejas</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.bebidas} subcategoria={"cervejas"}/>
+            <div className='subsecao'>
+              <h3>Vinhos</h3>
+              <hr></hr>
+            </div>
+            <Prato pratos={pratos.bebidas} subcategoria={"vinhos"}/>
 
         </div>
      <Footer/>
--- Exercicios/Semana07/Exercicio04/DEVINBurguer/src/pages/Prato/Prato.jsx
+++ Exercicios/Semana07/Exercicio04/DEVINBurguer/src/pages/Prato/Prato.jsx
@@ -1,35 +1,25 @@
 import './prato.css';
 import PropType from 'prop-types';
 
-export const Prato =({nome, pratos, subcategoria}) => {
+export const Prato =({pratos, subcategoria}) => {
     function verificaSubcategoria(subcategoria,subcategoria2){
         if (subcategoria.toLowerCase()===subcategoria2)
         return true;
     }
     return(
-        <>
             <ul className='listaPratos'>
-                {pratos.map((prato) => (
-                    <>
-                    {
-                       (verificaSubcategoria(subcategoria,prato.subcategoria))?
-                    <li key={prato.id} className="cardPrato">
-
+                {pratos.map((prato) => verificaSubcategoria(subcategoria,prato.subcategoria)?(    
+                    <li className="cardPrato" key={prato.id}>
                         <div className='fotoPrato'>
-                            <img src={prato.foto}>
-                            </img>
+                            <img src={prato.foto}/>
                             <h2>{prato.nome}</h2>
                              {prato.tempoPreparo?<p>Tempo preparo:<strong>{prato.tempoPreparo}</strong></p>:''}
                         </div>
-                    </li>:''}
-                    </>
-                ))}
+                    </li>):'')}
             </ul>
-        </>
     )
   }
 Prato.propTypes ={
-     nome: PropType.string.isRequired,
      pratos: PropType.array.isRequired,
      subcategoria: PropType.string.isRequired,
 }
